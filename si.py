863c66b3ab1d769fcb8ce5dfb59285e15912ca6a
vinayak_hubballi <vinayak.g.hubballi@gmail.com>
2025-10-23 20:20:36 +0530

    Save

---
+++ si.py
@@ -0,0 +1,11 @@
+# simple_interest.py
+
+# Input from user
+p = float(input("Enter principal amount: "))
+r = float(input("Enter rate of interest: "))
+t = float(input("Enter time in years: "))
+
+# Simple Interest Formula
+si = (p * r * t) / 100
+
+print("Simple Interest = ", si)
